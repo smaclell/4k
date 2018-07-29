2bd4240af893a16cf4272e96c229d8a12094e7a5
Scott MacLellan <scott.maclellan@vidyard.com>
2018-07-28 20:47:42 -0400

    Simple endpoints for getting data back

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ global.Promise = require('bluebird');
 const express = require('express');
 const bodyParser = require('body-parser');
 const createError = require('http-errors');
+const fs = require('fs');
+const path = require('path');
+
+const regions = require('../data/regions');
 
 const logger = console;
 const app = express();
@@ -18,6 +22,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/status', (req, res) => res.status(200).json({ ok: true }));
+
+app.get('/regions/:regionKey', (req, res, next) => {
+  const { regionKey } = req.params;
+  if (!regionKey) {
+    return next(createError(400, 'Missing :regionKey'));
+  }
+
+  const region = regions[regionKey];
+  if (!region) {
+    return next(createError(404, 'not found'));
+  }
+
+  return res.status(200).json(region);
+});
+
+app.get('/geojson/:regionKey', (req, res, next) => {
+  const { regionKey } = req.params;
+  if (!regionKey) {
+    return next(createError(400, 'Missing :regionKey'));
+  }
+
+  const filePath = path.join(__dirname, '..', 'data', 'geojson', `${regionKey}.json`);
+  return fs.exists(filePath, (exists) => {
+    if (!exists) {
+      return next(createError(404, 'not found'));
+    }
+
+    return res.status(200).sendFile(filePath);
+  });
+});
 
 app.use((req, res, next) => next(createError(404, 'not found')));
 
