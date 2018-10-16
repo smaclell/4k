1ed18c10c4a0cfc7b919876d6ce20c4a592b16ad
Justin Fangrad <justin.fangrad@shopify.com>
2018-10-16 19:14:08 -0400

    Reverse changes to geojson

--- server/api/geojson.js
+++ server/api/geojson.js
@@ -8,12 +8,12 @@ exports.get = (req, res, next) => {
     return next(createError(400, 'Missing :regionKey'));
   }
 
-  const filePath = path.join(__dirname, '..', '..', 'data', 'geojson', 'USA-HIA.json'); // `${regionKey}.json`
+  const filePath = path.join(__dirname, '..', '..', 'data', 'geojson', `${regionKey}.json`);
   return fs.exists(filePath, (exists) => {
     if (!exists) {
       return next(createError(404, 'not found'));
     }
-    console.log('sending', filePath);
+
     return res.status(200).sendFile(filePath);
   });
 };
